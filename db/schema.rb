c9c7f4535dadd7e2663e6ebf8b911f1446f838e0
Alex Pahom <alex.pahomchik@gmail.com>
2023-07-04 09:52:03 +0200

    feat(db): fix answer table, add null and default constraints

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_04_073849) do
+ActiveRecord::Schema.define(version: 2023_07_04_075045) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body"
+    t.boolean "correct", default: false
     t.integer "question_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -21,7 +22,7 @@ ActiveRecord::Schema.define(version: 2023_07_04_073849) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -35,8 +36,8 @@ ActiveRecord::Schema.define(version: 2023_07_04_073849) do
   end
 
   create_table "tests", force: :cascade do |t|
-    t.string "title"
-    t.integer "level"
+    t.string "title", null: false
+    t.integer "level", default: 1
     t.integer "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -44,7 +45,7 @@ ActiveRecord::Schema.define(version: 2023_07_04_073849) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
