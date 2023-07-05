ea599a782f9341ad814a120e2ade51b4938d2a97
Alex Pahom <alex.pahomchik@gmail.com>
2023-07-05 16:58:25 +0200

    find_tests_by_level

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+
+  def find_tests_by_level(level)
+    test_ids = Attempt.where('user_id = ? AND started_at IS NOT NULL', id).pluck(:test_id)
+    Test.where(id: test_ids, level: level)
+  end
 end
