216619a7892a0908059b1276a2d3e361fb37f26e
Alex Pahom <alex.pahomchik@gmail.com>
2023-07-08 00:31:45 +0200

    feature(assoc): test has many users

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,6 +3,7 @@ class Test < ApplicationRecord
   has_many :questions
   has_many :attempts
   belongs_to :author, class_name: 'User', foreign_key: :author_id
+  has_many :users, through: :attempts
 
   def self.find_by_category_name(name)
     joins(:category).where(categories: {title: name}).order(title: :desc).pluck(:title)
