cedd62cb8345b1e4dde66fca17ae11f537258bfb
alexpahom <31284624+alexpahom@users.noreply.github.com>
2023-07-09 12:59:46 +0200

    Feature/model associations (#4)
    * feature(assoc): add associations, refactor model methods
    
    * feature(assoc): test has many users
    
    * fix(assoc): CR - cascade destroy for models, nullify tests.author_id
    
    ---------
    
    Co-authored-by: Alex Pahom <alex.pahomchik@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
+  has_many :authored_tests, foreign_key: :author_id, class_name: 'Test', dependent: :nullify
+  has_many :attempts, dependent: :destroy
+  has_many :tests, through: :attempts, dependent: :destroy
 
   def find_tests_by_level(level)
-    Test.joins('JOIN attempts ON attempts.test_id = tests.id')
-        .where(attempts: { user_id: id }, level: level)
-        .distinct
+    tests.where(level: level).distinct
   end
 end
