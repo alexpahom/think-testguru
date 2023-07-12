ce07a277d9312696a5e6208ca86014aecf642c7a
alexpahom <31284624+alexpahom@users.noreply.github.com>
2023-07-12 18:08:47 +0200

    feature: question controller (#6)
    * feature(assoc): set controllers, fix node conflict
    
    * fix(controller): CR fixes
    
    * fix(controller): revert test_question_path, add id
    
    * fix(controller): CR fixes
    
    ---------
    
    Co-authored-by: Alex Pahom <alex.pahomchik@gmail.com>

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,51 @@
+class QuestionsController < ApplicationController
+
+  before_action :find_test, only: %i[index new create]
+  before_action :find_question, only: %i[show destroy]
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
+  def index
+    render plain: @test.questions.to_json
+  end
+
+  def show
+    render plain: @question.to_json
+  end
+
+  def new
+    @question = @test.questions.new
+  end
+
+  def create
+    question = @test.questions.new(question_params)
+    if question.save
+      redirect_to question_path(question.id)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @question.destroy
+    render plain: 'Тест удален'
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:body)
+  end
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
+  end
+
+  def rescue_with_question_not_found
+    render plain: 'Вопрос не найден'
+  end
+end
