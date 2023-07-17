e5398649759c95a92498cc6d130a22a957efa095
Alex Pahom <alex.pahomchik@gmail.com>
2023-07-17 22:35:14 +0200

    feature(auth): add user session

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
 
 
   get :signup, to: 'users#new'
+  get :login, to: 'sessions#new'
+
   resources :users, only: :create
+  resources :sessions, only: :create
 
   resources :tests do
     resources :questions, shallow: true, except: %i[index] do
