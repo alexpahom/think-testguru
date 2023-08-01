ee7d1c6d61fdf63fc967fe6c9b1743b6065f77f9
Alex Pahom <alex.pahomchik@gmail.com>
2023-08-01 03:08:08 +0200

    feature(prod): mailer fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,8 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = { host: 'alexpahom-test-guru-43bf94ca3a63.herokuapp.com' }
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
