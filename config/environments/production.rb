1c1e06b2d2b1295d5d151a2b8443cdacaee99288
Alex Pahom <alex.pahomchik@gmail.com>
2023-08-01 02:04:35 +0200

    feature(prod): cleanup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smpt_settings = {
+  config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
     user_name: ENV['SMTP_USERNAME'],
