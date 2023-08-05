a8388d9e5f408081a1f03ad69b9f80371101e907
Alex Pahom <alex.pahomchik@gmail.com>
2023-08-06 00:47:44 +0200

    feature(badge): add remaining actions & views

--- app/controllers/admin/badges_controller.rb
+++ app/controllers/admin/badges_controller.rb
@@ -1,8 +1,14 @@
 class Admin::BadgesController < ApplicationController
 
+  before_action :find_badge, only: %i[show edit update destroy]
+
   def index
     @badges = Badge.all
   end
+
+  def show; end
+
+  def edit; end
 
   def new
     @badge = Badge.new
@@ -17,7 +23,24 @@ class Admin::BadgesController < ApplicationController
     end
   end
 
+  def update
+    if @badge.update(badge_params)
+      redirect_to [:admin, @badge], success: 'ok'
+    else
+      redirect_to edit_admin_badge_path(@badge), alert: 'error'
+    end
+  end
+
+  def destroy
+    @badge.destroy
+    redirect_to admin_badges_path, success: 'deleted'
+  end
+
   private
+
+  def find_badge
+    @badge = Badge.find(params[:id])
+  end
 
   def badge_params
     params.require(:badge).permit(:name, :description, :rule_options, :badge_image_id, :rule_template_id)
