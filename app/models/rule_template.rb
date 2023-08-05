e9d03ab73342c944db451b90d0bb90ce89a0acab
Alex Pahom <alex.pahomchik@gmail.com>
2023-08-06 00:06:47 +0200

    feature(badge): disable unconfigurable rule if exists

--- app/models/rule_template.rb
+++ app/models/rule_template.rb
@@ -4,4 +4,8 @@ class RuleTemplate < ApplicationRecord
   has_many :badges
 
   validates :text, presence: true
+
+  def available?
+    option_key ? true : Badge.find_by(rule_template: self).nil?
+  end
 end
