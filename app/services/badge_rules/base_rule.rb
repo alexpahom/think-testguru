a034d7bb7ee509cf09e0f0aedc9445f76f391d34
Alex Pahom <alex.pahomchik@gmail.com>
2023-08-07 02:23:44 +0200

    feature(badge): cleanup

--- app/services/badge_rules/base_rule.rb
+++ app/services/badge_rules/base_rule.rb
@@ -4,7 +4,6 @@ module BadgeRules
   class BaseRule
     def initialize(passage, badge)
       @test_passage = passage
-      @rule = badge.rule_template # TODO: need?
       @rule_options = badge.rule_options
     end
   end
